585d419a43ea7ba961876d0268d8f2fbd8ac5601
Arjun Brar <arjun@outlook.ie>
2020-03-23 11:58:12 -0700

    Added extract
    Added the extract functionality

--- binwalkgui.py
+++ binwalkgui.py
@@ -38,6 +38,23 @@ def analyze():
 	else: 
 		messagebox.showerror("Error", "Please choose a file before analyzing")
 
+def extract():
+	files_extracted = ""
+	if label_name.cget("text") is not "":
+		for module in binwalk.scan(label_name.cget("text"), signature=True, extract=True):
+			for result in module.results:
+				if result.file.path in module.extractor.output:
+					# These are files that binwalk carved out of the original firmware image, a la dd
+					if result.offset in module.extractor.output[result.file.path].carved:
+						print ("Carved data from offset 0x%X to %s" % (result.offset, module.extractor.output[result.file.path].carved[result.offset]))
+						files_extracted += module.extractor.output[result.file.path].carved[result.offset]
+						files_extracted += "\n"
+					# These are files/directories created by extraction utilities (gunzip, tar, unsquashfs, etc)
+					if result.offset in module.extractor.output[result.file.path].extracted:
+						print ("Extracted %d files from offset 0x%X to '%s' using '%s'" % (len(module.extractor.output[result.file.path].extracted[result.offset].files),result.offset,module.extractor.output[result.file.path].extracted[result.offset].files[0],module.extractor.output[result.file.path].extracted[result.offset].command))
+
+		messagebox.showinfo("Files carved", "The following files have been carved: \n" + files_extracted)
+
 def choose_file(): 
 	label_results_data.configure(text='')
 	file_name = filedialog.askopenfilename()
@@ -47,6 +64,9 @@ def choose_file():
 # Generating buttons
 execute_button = Button(window, text="Analyze File", command=analyze)
 execute_button.grid(column=1, row=5)
+
+extract_button = Button(window, text="Extract File", command=extract)
+extract_button.grid(column=2, row=5)
 
 choose_file_button = Button(window, text="Choose file to analyze", command=choose_file)
 choose_file_button.grid(column=2, row=0)
